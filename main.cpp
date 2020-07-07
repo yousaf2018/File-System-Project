29aec969ed527f5e6f0755b7d8667df996c1f12f
yousaf2018 <yousaf2018@gmail.com>
2020-07-07 05:18:35 -0400

    Creation ,Display,Deletion in file system done with proper testing by Mahmood Yousaf

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ class FileAllocationTable{
         FileAllocationTable(){
         name = "None";
         indexList = NULL;
+        lengthIndex = -1;
         }
     //Check file name already exists or not
         int checkFileName(string name){
@@ -31,7 +32,6 @@ class FileAllocationTable{
         void InitializeIndexList_and_Name(int LenTex,string name){
             this->name = name;
             lengthIndex = LenTex;
-            cout << "Length " << lengthIndex << endl;
             indexList = new int[LenTex];
             int i;
             for(i=0;i<LenTex;i++){
@@ -40,8 +40,12 @@ class FileAllocationTable{
             return;
         }
     //Get file name
-        void get_file_name(){
-            cout << "File name is " << name << endl;
+        string get_file_name(){
+            return name;
+        }
+    //Set file name
+        void set_file_name(string name){
+            this->name = name;
         }
     //Get list 
         int* get_list(){
@@ -50,6 +54,15 @@ class FileAllocationTable{
     //Get list size
         int get_list_size(){
             return lengthIndex;
+        }
+    //Search file name
+        int searchFile(string name){
+            if(this->name==name){
+                return 1;
+            }
+            else{
+                return 0;
+            }
         }
 };
 //Class for to store data in file system 
@@ -72,8 +85,6 @@ class Data{
                 int *ptr,sizeList,i,counter=0;
                 ptr =  object.get_list();
                 sizeList = object.get_list_size();
-                cout << "Size of list " << sizeList << endl;
-                cout << actualData[99] << endl; 
                 for(i=0;i<100;i++){
                     if(counter==sizeList){
                         cout << "File is stored successfully\n";
@@ -81,8 +92,7 @@ class Data{
                     }
                     else if(bitMap[i]==-1){
                         actualData[i]=text[counter];
-                        cout << text[counter] << endl;
-                        ptr[i] = i;
+                        ptr[counter] = i;
                         bitMap[i] = 1;
                         counter++;
                     }
@@ -91,7 +101,31 @@ class Data{
                     }
                 }
         }
+    //Reading data from storage
+    void readData(FileAllocationTable object){
+        int listLength,*List,i;
+        cout << "Below is the data stored in our file system\n";
+        listLength = object.get_list_size();
+        List = object.get_list();
+        for(i=0;i<listLength;i++){
+            cout << actualData[List[i]];
+        }
+        cout << "\n";
 
+    }
+    //Delete data from storage 
+    void deleteData(FileAllocationTable object){
+        int listLength,*List,i;
+        listLength = object.get_list_size();
+        List = object.get_list();
+        object.set_file_name("None");
+        for(i=0;i<listLength;i++){
+            actualData[List[i]]='@';
+            bitMap[List[i]]=-1;
+        }
+        
+        cout << "File is deleted sucessfully\n";
+    }
 
 };
 
@@ -102,6 +136,9 @@ int main(){
     Data storage;
     while(true){
     cout << "Enter 1 for create file\n";
+    cout << "Enter 2 for search file\n";
+    cout << "Enter 3 for display file data\n";
+    cout << "Enter 4 for delete file\n";
     cin >> choice;
 //Method for creation of file
         if(choice == "1"){
@@ -136,7 +173,6 @@ int main(){
             cout << "Enter some text data for file\n";
             getline(cin,text);
             LenTex = text.size();
-            cout << "Last index " << text[6] << endl;
             //Initialize Index list with length of user input text
             Files[fileNumber].InitializeIndexList_and_Name(LenTex,name);
             //Writing data into storage
@@ -150,6 +186,62 @@ int main(){
             }
 
         }
+        //Method for search file in file system
+        else if(choice=="2"){
+            cout << "Enter file name for search\n";
+            cin.ignore();
+            getline(cin,name);
+            cout << name << endl;
+            for(i=0;i<100;i++){
+                check = Files[i].searchFile(name);
+                if(check==1){
+                    cout << "<<<We find your file in our system>>>\n";
+                    cout << Files[i].get_file_name() << endl;
+                    break;
+                }
+                else if(i==99){
+                    cout << "We donot find your file in our system\n";
+                }
+            }
+        }
+        //Method for display file from storage 
+        else if(choice=="3"){
+            cout << "Enter file name for display data\n";
+            cin.ignore();
+            getline(cin,name);
+            cout << name << endl;
+            for(i=0;i<100;i++){
+                check = Files[i].searchFile(name);
+                if(check==1){
+                    cout << "<<<We find your file in our system>>>\n";
+                    storage.readData(Files[i]);
+                    break;
+                }
+                else if(i==99){
+                    cout << "We donot find your file in our system\n";
+                }
+            }
+        }
+        //Method for delete file
+        else if(choice=="4"){
+            cout << "Enter file name for delete data\n";
+            cin.ignore();
+            getline(cin,name);
+            cout << name << endl;
+            for(i=0;i<100;i++){
+                check = Files[i].searchFile(name);
+                if(check==1){
+                    cout << "<<<We find your file in our system>>>\n";
+                    storage.deleteData(Files[i]);
+                    Files[i].set_file_name("None");
+                    break;
+                }
+                else if(i==99){
+                    cout << "We donot find your file in our system\n";
+                }
+            }
+        }
+        
     }    
     return 0;
 }
