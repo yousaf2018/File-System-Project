7f3ba5f2f5f3665a9d55c175075c3144847762ae
yousaf2018 <yousaf2018@gmail.com>
2020-07-07 03:10:46 -0400

    File iscreated and successfully stored

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ class FileAllocationTable{
     private:
         string name;
         int* indexList;
+        int lengthIndex;
     public:
     //Constructor to create new file with name and index list
         FileAllocationTable(){
@@ -29,6 +30,8 @@ class FileAllocationTable{
     //Initialize index list with length of user input text
         void InitializeIndexList_and_Name(int LenTex,string name){
             this->name = name;
+            lengthIndex = LenTex;
+            cout << "Length " << lengthIndex << endl;
             indexList = new int[LenTex];
             int i;
             for(i=0;i<LenTex;i++){
@@ -40,7 +43,14 @@ class FileAllocationTable{
         void get_file_name(){
             cout << "File name is " << name << endl;
         }
-
+    //Get list 
+        int* get_list(){
+            return indexList;
+        }
+    //Get list size
+        int get_list_size(){
+            return lengthIndex;
+        }
 };
 //Class for to store data in file system 
 class Data{
@@ -53,44 +63,62 @@ class Data{
         Data(){
             int i;
             for(i=0;i<100;i++){
-                actualData[i]='-1';
+                actualData[i]='@';
                 bitMap[i]=-1;
             }
         }
     //To writing data of file in storage
-        void writing_data(FileAllocationTable object){
-            
-            
+        void writing_data(FileAllocationTable object,string text){
+                int *ptr,sizeList,i,counter=0;
+                ptr =  object.get_list();
+                sizeList = object.get_list_size();
+                cout << "Size of list " << sizeList << endl;
+                cout << actualData[99] << endl; 
+                for(i=0;i<100;i++){
+                    if(counter==sizeList){
+                        cout << "File is stored successfully\n";
+                        break;
+                    }
+                    else if(bitMap[i]==-1){
+                        actualData[i]=text[counter];
+                        cout << text[counter] << endl;
+                        ptr[i] = i;
+                        bitMap[i] = 1;
+                        counter++;
+                    }
+                    else if(counter<sizeList&&i==99){
+                        cout << "Storage is full\n";
+                    }
+                }
         }
 
 
 };
 
 int main(){
-    int choice,LenTex,i,check,fileNumber;
+    int LenTex,i,check,fileNumber,indicator=0;
+    string name,text,choice;
     FileAllocationTable Files[100];
     Data storage;
     while(true){
     cout << "Enter 1 for create file\n";
     cin >> choice;
 //Method for creation of file
-        if(choice == 1){
-            string name,text;
-            //checking does file name already exists or not
-            for(i=0;i<100;i++){
+        if(choice == "1"){
                 cout << "Enter name for file\n";
                 cin.ignore();
                 getline(cin,name);
+            //checking does file name already exists or not
+            for(i=0;i<100;i++){
                 check=Files[i].checkFileName(name);
                 if(check==-1){
-                    cout << "Enter another file name your input file already exists\n";
-                    fileNumber = i;
-                    cin.ignore();
-                    getline(cin,name);
-                    continue;
+                    cout << "Your input file name already exists\n";
+                    indicator = 0;
+                    break;
                 }
                 else if(check==1){
                     fileNumber = i;
+                    indicator = 1;
                     break;
                 }
                 else if(check==0){
@@ -98,18 +126,29 @@ int main(){
                 }
                 else if(i==99){
                     cout << "Our file system have no space for more files\n";
+                    indicator = -1;
                     return 1;
                 }
                 cout << i;
             }
+            if(indicator==1){
             //File creation with some text data
             cout << "Enter some text data for file\n";
             getline(cin,text);
             LenTex = text.size();
+            cout << "Last index " << text[6] << endl;
             //Initialize Index list with length of user input text
             Files[fileNumber].InitializeIndexList_and_Name(LenTex,name);
             //Writing data into storage
-            storage.writing_data(Files[fileNumber]);
+            storage.writing_data(Files[fileNumber],text);
+            }
+            else if(indicator==0){
+                cout << "\n<<<Enter unique file name>>>\n\n";
+            }
+            else if(indicator==-1){
+                cout << "File system have no more space delete extra files\n";
+            }
+
         }
     }    
     return 0;
